caf38f2f0a0ea515d68f649dd64d48ebad29b870
heladolk <heladolk@yahoo.com>
2016-06-26 16:53:03 -0700

    make event search return results and save to events db

--- routes/eventbrite.js
+++ routes/eventbrite.js
@@ -1,29 +1,25 @@
 "use strict"
 var request = require('request');
-var Events = require('./models/events');
+// var Events = require('../models/events');
 
-var keyword = 'hackathon';
-var location = 'san francisco';
-var radius = '10mi';
-var token = process.env.EVENTBRITE_ANON_OAUTH_TOKEN;
+function searchEventBriteEvents(keyword, location, radius) {
+  let url = `https://www.eventbriteapi.com/v3/events/search/?q=${keyword}&location.address=${location}&location.within=${radius}&token=${process.env.EVENTBRITE_ANON_OAUTH_TOKEN}`;
 
-searchEventBriteEvents();
-
-function searchEventBriteEvents() {
-  let url = `https://www.eventbriteapi.com/v3/events/search/?q=${keyword}&location.address=${location}&location.within=${radius}&token=${token}`;
-
-  request.get(url, function(err, res, body) {
-    body = JSON.parse(body);
-    // console.log(body.events[0]);
-    let events = body.events.map(function(event) {
-      return new Events(event);
-    });
-    console.log("events saved ============")
-    console.log(events);
-
-    // events.forEach(function(event) {
-    //   event.insert();
-    // });
-  });
-
+  return promisifyGet(url)
 }
+
+function promisifyGet(url) {
+    return new Promise(function(resolve, reject) {
+        request.get(url, function(error, response, body){
+            if(error) {
+                reject(error);
+            }
+            else {
+                resolve(response);
+            }
+        });
+    });
+}
+
+
+module.exports = searchEventBriteEvents;
--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,9 @@
 
 let express = require('express'),
     router = express.Router(),
-    knex = require('../db/knex');
+    knex = require('../db/knex'),
+    searchEventBriteEvents = require('./eventbrite'),
+    Events = require('../models/events');
 
 router.get('/', function(req, res, next) {
   knex('events')
@@ -11,35 +13,63 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/', function(req, res, next) {
-  knex('events')
-  .insert({
-    name: req.body.name,
-    url: req.body.url,
-    start_time: req.body.start_time,
-    end_time: req.body.end_time,
-    group_name: req.body.group_name,
-    venue: req.body.venue
-  })
-  .returning('*')
-  .then(function(addedEvent) {
-    res.json(addedEvent);
-  });
-});
+// router.post('/', function(req, res, next) {
+//   knex('events')
+//   .insert({
+//     name: req.body.name,
+//     url: req.body.url,
+//     start_time: req.body.start_time,
+//     end_time: req.body.end_time,
+//     group_name: req.body.group_name,
+//     venue: req.body.venue
+//   })
+//   .returning('*')
+//   .then(function(addedEvent) {
+//     res.json(addedEvent);
+//   });
+// });
 
 router.post('/search', function(req, res, next) {
-  console.log("inside events/search")
-  console.log(req.body);
-});
-
-router.delete('/:id', function(req, res, next) {
-  knex('events')
-  .where('events.id', req.params.id)
-  .del()
-  .returning('*')
-  .then(function(deletedEvent) {
-    res.json(deletedEvent);
+  let keyword = req.body.keyword;
+  let location = req.body.location;
+  let radius = req.body.radius || '5mi';
+  let events = searchEventBriteEvents(keyword, location, radius);
+  events.then(function(searchResponse){
+    let jsonBody = JSON.parse(searchResponse.body);
+    let events = jsonBody.events.map(function(event) {
+      return new Events(event);
+    });
+    let promises = [];
+    events.forEach(function(event) {
+      let query = knex('events')
+      .insert({
+        name: event.name,
+        url: event.url,
+        start_time: event.start_time,
+        end_time: event.end_time,
+        group_name: event.group_name,
+        venue: event.venue
+      }).returning('*')
+      promises.push(query);
+    });
+    return Promise.all(promises);
+  })
+  .then(function(data) {
+    res.redirect('/events');
+  })
+  .catch(function(error) {
+    console.log(error);
   });
 });
+
+// router.delete('/:id', function(req, res, next) {
+//   knex('events')
+//   .where('events.id', req.params.id)
+//   .del()
+//   .returning('*')
+//   .then(function(deletedEvent) {
+//     res.json(deletedEvent);
+//   });
+// });
 
 module.exports = router;
