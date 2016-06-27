aad65b6ccad621f8b53c30ba70fbb05e3ed80673
heladolk <heladolk@yahoo.com>
2016-06-27 07:58:32 -0700

    refactor sendgrid.js to use .env file

--- routes/sendgrid.js
+++ routes/sendgrid.js
@@ -1,16 +1,13 @@
 "use strict";
 
-let SENDGRID_API_KEY = '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';
-
 let helper = require('sendgrid').mail
 let from_email = new helper.Email("test@example.com")
 let to_email = new helper.Email("heladolk@yahoo.com")
 let subject = "Happy Monday Morning"
-let content = new helper.Content("text/plain", "This is very cool!")
+let content = new helper.Content("text/plain", "Sending an email using sendgrid is very fun")
 let mail = new helper.Mail(from_email, subject, to_email, content)
 
-// let sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
-let sg = require('sendgrid').SendGrid(SENDGRID_API_KEY)
+let sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
 let requestBody = mail.toJSON()
 let request = sg.emptyRequest()
 request.method = 'POST'
