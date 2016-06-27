1ae07a2f64199d4d805a588148caf8e34b286b01
heladolk <heladolk@yahoo.com>
2016-06-27 09:55:39 -0700

    minor refactor

--- routes/sendgrid.js
+++ routes/sendgrid.js
@@ -2,9 +2,9 @@
 
 let helper = require('sendgrid').mail
 let from_email = new helper.Email("test@example.com")
-let to_email = new helper.Email("heladolk@yahoo.com")
+let to_email = new helper.Email("dainguyen@gmail.com")
 let subject = "Happy Monday Morning"
-let content = new helper.Content("text/plain", "Sending an email using sendgrid is very fun")
+let content = new helper.Content("text/plain", "Hey Dai!")
 let mail = new helper.Mail(from_email, subject, to_email, content)
 
 let sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY)
