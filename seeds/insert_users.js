353127fbe47a642dc15ef77cedff962aad8873fe
heladolk <heladolk@yahoo.com>
2016-06-28 14:46:16 -0700

    clean up

--- seeds/insert_users.js
+++ seeds/insert_users.js
@@ -8,9 +8,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('users').insert({username: 'clintworrell', password: bcrypt.hashSync("pass1", 10), firstname: 'Clint', lastname: 'Worrell', email: 'clint@example.com'}),
+        knex('users').insert({username: 'clintworrell', password: bcrypt.hashSync("pass1", 10), firstname: 'Clint', lastname: 'Worrell', email: 'clint.worrell@gmail.com'}),
         knex('users').insert({username: 'd', password: bcrypt.hashSync("pass2", 10), firstname: 'Dai', lastname: 'Nguyen', email: 'dainguyen@gmail.com'}),
-        knex('users').insert({username: 'tuhmaytow', password: bcrypt.hashSync("pass3", 10), firstname: 'Nica', lastname: 'Lazatin', email: 'nica@example.com'}),
+        knex('users').insert({username: 'tuhmaytow', password: bcrypt.hashSync("pass3", 10), firstname: 'Nica', lastname: 'Lazatin', email: 'tuhmaytow@gmail.com'}),
         knex('users').insert({username: 'winniekwon', password: bcrypt.hashSync("pass4", 10), firstname: 'Winnie', lastname: 'Kwon', email: 'heladolk@yahoo.com'})
       ]);
     });
--- worker_reminder.js
+++ worker_reminder.js
@@ -3,18 +3,12 @@ let knex = require('./db/knex'),
     helper = require('sendgrid').mail,
     sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY);
 
-// worker connects to postgres and searches events that starts within 24hrs from events table
-// then worker get list of users who saved the events
 let now = new Date();
-// let now = new Date('2018-06-28 14:00:00-07');
 let nowPlus24hrs = new Date(now.getTime() + (24 * 60 * 60 * 1000));
-console.log(now);
-console.log(nowPlus24hrs);
 
 knex('events').select().where('start_time', '>=', now).andWhere('start_time', '<=', nowPlus24hrs).returning('*')
 .then( (events) => {
-  console.log(events);
-
+  // console.log(events);
   let usersPromises = [];
   events.forEach( (event) => {
     let eventId = event.id;
@@ -29,6 +23,7 @@ knex('events').select().where('start_time', '>=', now).andWhere('start_time', '<
 .then( (eventLists) => {
   eventLists.forEach( (events) => {
     events.forEach ( (userEvent) => {
+      // console.log(userEvent);
       sendReminderEmail(userEvent);
     });
   });
@@ -36,15 +31,13 @@ knex('events').select().where('start_time', '>=', now).andWhere('start_time', '<
 
 function sendReminderEmail(userEvent) {
   let user_email = userEvent.email;
-  console.log(user_email);
-  // compose reminder email
+
   let from_email = new helper.Email("event-listener@example.com")
   let to_email = new helper.Email(user_email);
   let subject = `Event reminder: ${userEvent.name}`;
   let content = new helper.Content("text/plain", `${userEvent.name} will happen ${userEvent.start_time} at ${userEvent.venue}`);
   let mail = new helper.Mail(from_email, subject, to_email, content)
 
-  // send email
   let requestBody = mail.toJSON()
   let request = sg.emptyRequest()
   request.method = 'POST'
