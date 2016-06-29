a9d5952dc5cea6cbe6c93d8572d3fc5dcf4070bc
eunica lazatin <NicaLazatin@eunicas-MacBook-Pro.local>
2016-06-29 13:05:09 -0700

    finalized oauth

--- routes/auth.js
+++ routes/auth.js
@@ -11,11 +11,6 @@ cookieSession = require('cookie-session'),
 knex = require('../db/knex'),
 passport = require('passport');
 
-// router.use(passport.initialize());
-// router.use(passport.session());
-
-
-
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_API_KEY,
   clientSecret: process.env.GOOGLE_SECRET_KEY,
@@ -23,10 +18,7 @@ passport.use(new GoogleStrategy({
   enableProof: true
 },
 function(request, accessToken, refreshToken, profile, done) {
-console.log(profile);
-
   knex('users').where({email:profile.emails[0].value, username:profile.emails[0].value.split('@')[0]}).first().then(function(user) {
-    console.log(user);
     if(!user) {
       knex('users').insert({
         email: profile.emails[0].value,
@@ -42,34 +34,17 @@ console.log(profile);
     }
     else {
       console.log('USER ALREADY EXIST', user);
-      //manage cookie;
-      //res.cookie();
       return done(null, user);
     }
-  }). catch(function(error) {
+  })
+  .catch(function(error) {
     return done(error, null);
   });
 
 }));
 
-//
-// passport.serializeUser(function(user, done) {
-//   done(null, user.id);
-// });
-//
-// passport.deserializeUser(function(id, done) {
-//   done(null, id);
-// });
-
 
 router.get('/google',
-  // function(req, res, next) {
-  // req.logout();
-  // res.clearCookie('session');
-  // res.clearCookie('session.sig');
-  // res.clearCookie('userID');
-  // next();
-  // },
   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read'] })
 );
 
@@ -77,7 +52,8 @@ router.get('/google',
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    req.session.user = req.user;
+    req.session.id = req.user.id;
+    req.session.username = req.user.username;
     res.redirect(`/users/${req.user.id}`);
   }
 );
