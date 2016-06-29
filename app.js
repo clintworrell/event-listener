e31f35e3821056b4fa9d244f4cd61fc6c656bfcd
heladolk <heladolk@yahoo.com>
2016-06-29 10:57:10 -0700

    change favicon location

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieSession({
 }));
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/public/img/favicons/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
