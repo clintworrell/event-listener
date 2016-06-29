dced4d12c1e00de7bbbf85c98821fa89a0b0ff8e
heladolk <heladolk@yahoo.com>
2016-06-29 11:29:03 -0700

    move favicon to /img

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieSession({
 }));
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, '/public/favicomatic/favicon.ico')));
+app.use(favicon(path.join(__dirname, '/public/img/favicomatic/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
