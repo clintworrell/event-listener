db9c55cfbb8b068c0edc45319831343f45b55b60
heladolk <heladolk@yahoo.com>
2016-06-29 16:14:28 -0700

    change reminder email from plain text to html

--- worker_reminder.js
+++ worker_reminder.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict';
+
 let knex = require('./db/knex'),
     helper = require('sendgrid').mail,
     sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY);
@@ -8,7 +9,6 @@ let nowPlus24hrs = new Date(now.getTime() + (24 * 60 * 60 * 1000));
 
 knex('events').select().where('start_time', '>=', now).andWhere('start_time', '<=', nowPlus24hrs).returning('*')
 .then( (events) => {
-  // console.log(events);
   let usersPromises = [];
   events.forEach( (event) => {
     let eventId = event.id;
@@ -23,7 +23,7 @@ knex('events').select().where('start_time', '>=', now).andWhere('start_time', '<
 .then( (eventLists) => {
   eventLists.forEach( (events) => {
     events.forEach ( (userEvent) => {
-      // console.log(userEvent);
+      console.log(userEvent);
       sendReminderEmail(userEvent);
     });
   });
@@ -35,14 +35,22 @@ function sendReminderEmail(userEvent) {
   let from_email = new helper.Email("event-listener@example.com")
   let to_email = new helper.Email(user_email);
   let subject = `Event reminder: ${userEvent.name}`;
-  let content = new helper.Content("text/plain", `${userEvent.name} will happen ${userEvent.start_time} at ${userEvent.venue}`);
+  let content = new helper.Content("text/html",
+        `<html>
+            <h1 style="color:red;"><em>${userEvent.name}</em></h2>
+            <h2>Organizer: ${userEvent.organizer_name}</h4>
+            <hr>
+            <h4>Start: ${userEvent.start_time}</h4>
+            <h4>Venue: ${userEvent.venue}</h4>
+            <a href="${userEvent.url}">${userEvent.url}</a>
+        </html>`
+  );
   let mail = new helper.Mail(from_email, subject, to_email, content)
 
-  let requestBody = mail.toJSON()
   let request = sg.emptyRequest()
   request.method = 'POST'
   request.path = '/v3/mail/send'
-  request.body = requestBody
+  request.body = mail;
   sg.API(request, function (response) {
     console.log(response.statusCode)
     console.log(response.body)
