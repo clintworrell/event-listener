d91e59ffbf50660685d248d4da971fd6357d7737
heladolk <heladolk@yahoo.com>
2016-06-29 18:08:04 -0700

    update with promise.all

--- worker_reminder.js
+++ worker_reminder.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// require('dotenv').load();
 let knex = require('./db/knex'),
     helper = require('sendgrid').mail,
     sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY);
@@ -7,7 +7,8 @@ let knex = require('./db/knex'),
 let now = new Date();
 let nowPlus24hrs = new Date(now.getTime() + (24 * 60 * 60 * 1000));
 
-knex('events').select().where('start_time', '>=', now).andWhere('start_time', '<=', nowPlus24hrs).returning('*')
+knex('events').select()
+.where('start_time', '>=', now).andWhere('start_time', '<=', nowPlus24hrs).returning('*')
 .then( (events) => {
   let usersPromises = [];
   events.forEach( (event) => {
@@ -15,18 +16,24 @@ knex('events').select().where('start_time', '>=', now).andWhere('start_time', '<
     let userQuery = knex.select().from('users')
                 .innerJoin('users_events', 'users_events.user_id', 'users.id')
                 .innerJoin('events', 'events.id', 'users_events.event_id')
-                .where('events.id', '=', eventId);
+                .where('events.id', '=', eventId).andWhere('users_events.reminder', '=', false);
     usersPromises.push(userQuery);
   });
   return Promise.all(usersPromises);
 })
 .then( (eventLists) => {
+  let emailPromises = [];
   eventLists.forEach( (events) => {
     events.forEach ( (userEvent) => {
       console.log(userEvent);
-      sendReminderEmail(userEvent);
+      let sendEmailPromise = sendReminderEmail(userEvent);
+      emailPromises.push(sendEmailPromise);
     });
-  });
+  })
+  return Promise.all(emailPromises);
+})
+.then( () => {
+  process.exit(0);
 });
 
 function sendReminderEmail(userEvent) {
@@ -37,25 +44,57 @@ function sendReminderEmail(userEvent) {
   let subject = `Event reminder: ${userEvent.name}`;
   let content = new helper.Content("text/html",
         `<html>
-            <h1 style="color:red;"><em>${userEvent.name}</em></h2>
+          <head>
+            <style>
+              body {
+                text-align: center;
+              }
+              h1 {
+                color: red;
+              }
+            </style>
+          </head>
+          <body>
+            <h1><em>${userEvent.name}</em></h2>
             <h2>Organizer: ${userEvent.organizer_name}</h4>
             <hr>
             <h4>Start: ${userEvent.start_time}</h4>
             <h4>Venue: ${userEvent.venue}</h4>
             <a href="${userEvent.url}">${userEvent.url}</a>
-        </html>`
+          </body>
+          </html>`
   );
+
   let mail = new helper.Mail(from_email, subject, to_email, content)
 
   let request = sg.emptyRequest()
   request.method = 'POST'
   request.path = '/v3/mail/send'
   request.body = mail;
-  sg.API(request, function (response) {
-    console.log(response.statusCode)
-    console.log(response.body)
-    console.log(response.headers)
+
+  return sendEmailPromisify(request)
+  .then( (response) => {
+    console.log(response);
+    if(response.statusCode === 202) {
+      console.log("reminder was sent successfully");
+      knex('users_events').where('user_id', '=', userEvent.user_id).andWhere('event_id', '=', userEvent.event_id)
+      .update({reminder: true}).returning('id')
+      .then( (userEventId) => {
+        console.log(`Updated reminder field`);
+      });
+    } else {
+      console.log("Failed to send reminder")
+    }
+
   });
+
+  function sendEmailPromisify(emailRequest) {
+    return new Promise((resolve, reject) => {
+      sg.API(emailRequest, (response) => {
+          resolve(response);
+      });
+    });
+  };
 
 }
 
